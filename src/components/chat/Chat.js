ad5ea4e9cc6bd417825b13ee41c5ff1b30faee34
seun jay <seunjay92@gmail.com>
2020-09-05 15:35:05 +0100

    build and style chat header

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import {ChatContainer} from "./chat.styles"
+import React from "react";
+import {
+  ChatContainer,
+  ChatHeader,
+  ChatInfoContainer,
+  ChatIconContainer,
+  ChatInfoName,
+  ChatInfoTimeStamp,
+} from "./chat.styles";
+import { Avatar, IconButton } from "@material-ui/core";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
 
 const Chat = () => {
-  return <ChatContainer>I am the chat component</ChatContainer>;
-}
+  return (
+    <ChatContainer>
+      <ChatHeader>
+        <Avatar />
 
-export default Chat
+        <ChatInfoContainer>
+          <ChatInfoName>Room Name</ChatInfoName>
+          <ChatInfoTimeStamp>Last seen ...</ChatInfoTimeStamp>
+        </ChatInfoContainer>
+
+        <ChatIconContainer>
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </ChatIconContainer>
+      </ChatHeader>
+    </ChatContainer>
+  );
+};
+
+export default Chat;
--- src/components/chat/chat.styles.js
+++ src/components/chat/chat.styles.js
@@ -6,5 +6,28 @@ export const ChatContainer = styled.div`
   flex: 0.65;
 `;
 
+export const ChatHeader = styled.div`
+  display: flex;
+  /* justify-content: space-between; */
+  align-content: center;
+  padding: 20px;
+  border: 1px solid lightgray;
+`;
 
+export const ChatInfoContainer = styled.div`
+  flex: 1;
+  padding-left: 40px;
+`;
 
+export const ChatInfoName = styled.h3`
+  margin-bottom: 3px;
+  font-weight: 500;
+`;
+export const ChatInfoTimeStamp = styled.p`
+  color: gray;
+`;
+
+export const ChatIconContainer = styled.div`
+  display: flex;
+  align-content: center;
+`;
