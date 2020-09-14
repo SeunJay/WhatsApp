31744d39e83245ae9509e79010253ad51e3c9420
seun jay <seunjay92@gmail.com>
2020-09-14 06:58:07 +0100

    implement sign in with Google functionality in Login component

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -7,12 +7,17 @@ import {
   LoginText,
 } from "./login.styles";
 import { auth, provider } from "../../firebase/firebase";
+import { useStateValue } from "../context/StateProvider";
+import { actionTypes } from "../reducer";
 import { Button } from "@material-ui/core";
 
 const Login = () => {
+  const [{}, dispatch] = useStateValue();
+
   const signIn = async () => {
     try {
       const data = await auth.signInWithPopup(provider);
+      dispatch({ type: actionTypes.SET_USERS, user: data.user });
       console.log(data);
     } catch (error) {
       console.log(error);
